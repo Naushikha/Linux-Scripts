5ff6715bbc681b3efeafd41b0af8e9d70526acf1
Naushikha <dev@naushikha.com>
2022-06-19 20:08:40 +0530

    Added a backup script

---
+++ BackupScript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+myDate="`date +"%Y_%m_%d"`"
+
+# Backup UCSC stuff
+echo Backing up UCSC stuff...
+rsync -avu --delete /mnt/secondary/UCSC/ /media/naush/EXT-HD/BACKUPS/UCSC > "$HOME/Desktop/${myDate}_BKUP_UCSC" 2>&1
+
+# Backup DOCS
+echo Backing up DOCS...
+rsync -avu --delete /mnt/secondary/DOCS/ /media/naush/EXT-HD/BACKUPS/DOCS > "$HOME/Desktop/${myDate}_BKUP_DOCS" 2>&1
+
+# Backup MUSIC
+echo Backing up MUSIC...
+rsync -avu --delete /mnt/secondary/MUSIC/ /media/naush/EXT-HD/BACKUPS/MUSIC > "$HOME/Desktop/${myDate}_BKUP_MUSIC" 2>&1
+
+echo Done.

0cdc5e1646b97f04749706cbb5edb265e070a4c0
Naushikha <dev@naushikha.com>
2022-06-29 11:40:47 +0530

    Added portable backup script: rsync.h

---
+++ rsync.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+myDate="$(date +"%Y_%m_%d_%H_%_M_%S")"
+
+DEST=$(zenity --file-selection --directory --title="Select a Backup Destination Folder")
+
+case $? in
+0)
+    echo "Destination directory '$DEST' selected."
+    zenity --question --text="Delete files in destination to match source?" --width=300
+    if [ $? = 0 ]; then
+        echo "Backing up '$PWD' into '$DEST'..."
+        rsync -avu --delete . "$DEST" >"$HOME/Desktop/${myDate}" 2>&1
+    else
+        echo "Backing up '$PWD' into '$DEST'..."
+        rsync -avu . "$DEST" >"$HOME/Desktop/${myDate}" 2>&1
+    fi
+    echo Done.
+    echo "Logs are at '$HOME/Desktop/${myDate}'"
+    ;;
+1)
+    echo "No folder selected."
+    ;;
+-1)
+    echo "An unexpected error has occurred."
+    ;;
+esac

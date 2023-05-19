e45276d8d027aa6a989f45cbddc08abdb9f816db
Naushikha Jayawickrama <dev@naushikha.com>
2023-05-19 14:35:22 +0530

    Added taskbar scripts

---
+++ Taskbar Scripts/Monitor/M2>HDMI.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# I2C Values
+# 2 = Monitor 1 (DisplayPort)
+# 4 = Monitor 2 (HDMI)
+
+# Values:
+#     01: VGA-1
+#     0f: DisplayPort-1
+#     11: HDMI-1
+
+
+ddcutil -b 4 setvcp 60 0x11  # HDMI
